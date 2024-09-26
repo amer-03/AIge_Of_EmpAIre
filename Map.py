26a3d30ce28e8f9b2753f58d050006668321f55e
Amer <amer.eljibbawe03@gmail.com>
2024-09-26 18:30:19 +0200

    update Main.py

--- Map.py
+++ Map.py
@@ -2,13 +2,33 @@ import random
 
 class Map(object):
     def __init__ (self,terrain_nature,ressource,player_position):
-        self.grid=[]
         self.size=120
+        self.grid=[[0 for _ in range(self.size)]for _ in range(self.size)]
         self.terrain_nature=terrain_nature
         self.ressource=ressource
         self.player_position=player_position
     
     def generate_map(self):
         for _ in range (self.size):
-            self.grid.append([])
+            x=random.randrange(0,self.size)
+            self.grid[x]=['~' for _ in range(self.size//6)]
+            
+    def __str__ (self):
+        return str(self.grid)
+
+mymap=Map("foret","bois",(0,0))
+mymap.generate_map()
+print(mymap)
+
+import pygame
+pygame.init()
+from pygame.locals import *
+
+fenetre = pygame.display.set_mode((200, 200))
+
+
+        
+
+
+
         
