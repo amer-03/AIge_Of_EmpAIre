4229bef80d442fe3e2fb81d7e2b36f5f2ddba918
Amer <amer.eljibbawe03@gmail.com>
2024-09-27 15:59:24 +0200

    aaaa

--- Game.py
+++ Game.py
@@ -1,14 +1,28 @@
 import pygame
+from Map import *
+#from Player import *
 
 class Game :
     def __init__(self):
         pygame.init()
         self.name=""
-        self.player=player
-        self.map=map
+        #self.player=Player
+        self.map=Map
         self.game_time=pygame.time.Clock
         self.running = True
         self.screen=pygame.display.set_mode((pygame.display.Info().current_w, pygame.display.Info().current_h),pygame.FULLSCREEN)
+    
+    def run(self):
+        while self.running:
+            for e in pygame.event.get():
+                if e.type==pygame.QUIT: self.running=False
+                #elif e.type == pygame.KEYDOWN: self.running=False
+        
+            self.screen.fill((139,69,19))
+        
+            pygame.display.flip()    
+
+Game().run()  
     #def start_game(self):
     #    if self.is_running:
     #        print("Le jeu '{self.name}' est déjà en route.")
