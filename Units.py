50ad39edcf2eefdad8ee129708f02fb44e102044
Amer <amer.eljibbawe03@gmail.com>
2024-12-24 01:47:39 +0100

    animation for the unit

--- Units.py
+++ Units.py
@@ -12,8 +12,8 @@ class Unit:
         self.direction_index = 0 # indice du frame dans une colonne
         self.last_time = pygame.time.get_ticks()
     
-    def animation(self,current_time):
-        if current_time - self.last_time > 5000//30: #1000//30: 30 sous-images par 1000 millisecondes
+    def animation(self,current_time):  # fonction qui modifie l'indice des frames et le dernier temps du frame
+        if current_time - self.last_time > 5000//30: #1000//30: 30 frames par 1000 millisecondes
             self.last_time = current_time
             self.frame_index = (self.frame_index + 1) % 30
     
@@ -22,28 +22,26 @@ class Unit:
 
         #coordonnées isométrique    
         iso_x, iso_y = self.coordinates.to_iso(cam_x, cam_y)
-        f"iso_x: {iso_x}, iso_y: {iso_y}"
 
         unit_image=  pygame.image.load(unit_tile).convert_alpha()
         
+        #appel de la fonction de l'animation
         self.animation(current_time)
         
-        frame_width = unit_image.get_width()//30 #30 nombre des sous images par lignes 
-        frame_height = unit_image.get_height()//16 #16 nombre des sous images par colonnes
-        f"frame_width: {frame_width}, frame_width: {frame_height}"
+        #calcul des tailles du chaque frame en divisant la taille de l'image principal par le nombre des frames 
+        frame_width = unit_image.get_width()//30 #30 nombre des frames par lignes 
+        frame_height = unit_image.get_height()//16 #16 nombre des frames par colonnes
 
-        #multiplication de l'indice du frame par la taille de chaque sous image pour chercher le vrai
+        #multiplication de l'indice du frame par la taille de chaque frame pour chercher les vrai coordonnees
         frame_x = self.frame_index * frame_width 
         frame_y = self.direction_index * frame_height
-        f"frame_x: {frame_x}, frame_y: {frame_y}"
-        f"frame_index: {self.frame_index}, direction_index: {self.direction_index}"
 
+        #enlever un frame de l'image principal et l'afficher a la fois
         frame_rect = pygame.Rect(frame_x,frame_y, frame_width, frame_height)
-        unit_sub_images =  unit_image.subsurface(frame_rect)
-        DISPLAYSURF.blit(unit_sub_images,(iso_x,iso_y))
-        
-unit=Unit()
-unit.diplay_unit(4,4,pygame.time.get_ticks())
+        unit_frame =  unit_image.subsurface(frame_rect)
+
+        DISPLAYSURF.blit(unit_frame,(iso_x,iso_y))
+
 
         
 
